05971bc1fec17671a0e85cd1f36443572e816ea6
DugDaniels <cdouglasd@gmail.com>
2021-12-16 11:12:04 -0500

    Add Label component

---
+++ packages/components/components/Label/Label.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { styled } from "../../stitches.config";
+
+export interface ILabelProps {
+  label?: string;
+  description?: string;
+  required?: boolean;
+  error?: string;
+  children?: React.ReactNode;
+}
+
+const Input = ({ label, description, required, error, children }: ILabelProps) => {
+  return (
+    <Container>
+      <LabelRow>
+        <Label>{label}</Label>
+        {required && <Required>(Required)</Required>}
+      </LabelRow>
+      {description && <Description>{description}</Description>}
+      {children}
+      {error && <Error>{error}</Error>}
+    </Container>
+  );
+}
+
+const Container = styled("div", {
+});
+
+const LabelRow = styled("div", {
+  display: "flex",
+  mb: "$1",
+});
+
+const Required = styled("div", {
+  fontFamily: "$mono",
+  fontSize: "$sm",
+  textTransform: "uppercase",
+  fontWeight: 400,
+  fontStyle: "italic",
+  ml: "$2",
+});
+
+const Label = styled("div", {
+  fontFamily: "$mono",
+  fontWeight: 600,
+  fontSize: "$sm",
+  textTransform: "uppercase",
+});
+
+const Description = styled("div", {
+  fontFamily: "$sans",
+  fontSize: "$sm",
+  mb: "$1",
+});
+
+const Error = styled("div", {
+  fontFamily: "$sans",
+  fontSize: "$sm",
+  color: "#FF0000",
+  mt: "$1",
+});
+
+
+export default Input;
