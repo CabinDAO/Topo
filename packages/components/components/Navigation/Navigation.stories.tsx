4b07af4d0b9cff843ee01bbb672e62784df395b2
DugDaniels <cdouglasd@gmail.com>
2022-01-05 14:03:36 -0500

    Add navigation component

---
+++ packages/components/components/Navigation/Navigation.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import { Story } from "@storybook/react";
+import Navigation from "./Navigation";
+import { Box } from "../";
+
+export default {
+  title: "Components/Navigation",
+  component: Navigation,
+  argTypes: {},
+  args: {
+    links: null,
+  },
+} as Meta;
+
+const Template: Story = (args) => (
+  <Box>
+    <Navigation {...args} />
+  </Box>
+);
+
+export const Standard = Template.bind({});
+Standard.args = {
+  links: [
+    { label: "About", href: "/#about" },
+    { label: "Products", href: "/products" },
+    { label: "Community", href: "/#community" },
+    { label: "Initiatives", href: "/#initiatives" },
+    { label: "Passports", href: "/#passports" },
+  ],};
---
+++ packages/components/components/Navigation/Navigation.tsx
@@ -0,0 +1,191 @@
+import React from "react";
+import { useState } from 'react'
+import { styled } from "../../stitches.config";
+import { HamburgerMenuIcon, Cross1Icon } from '@radix-ui/react-icons'
+import Button from '../Button';
+import Wrapper from "../Wrapper";
+
+export interface INavigationProps {
+  links?: { label: string, href: string }[];
+}
+
+export default function Header({ links }: INavigationProps) {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const nav = (
+    <Nav>   
+      {links && links.map(({ label, href }, i) => (
+        <a href={href} key={i}>{label}</a>
+      ))}
+    </Nav>
+  );
+
+  const joinButton = (
+    <Button tone="wheat" as="a" href="https://discord.gg/N6hVmFygjR">
+      Join the community
+    </Button>
+  );
+
+  const toggle = (
+    <MenuToggle onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+      {mobileMenuOpen ? <Cross1Icon /> : <HamburgerMenuIcon />}
+    </MenuToggle>
+  );
+
+  const mobileNav = ( 
+    <MobileNav>
+      <Container>
+        <Wordmark>
+          <a href="/">
+            Cabin
+          </a>
+        </Wordmark>
+        {toggle}
+      </Container>
+      <MobileMenu open={mobileMenuOpen}>
+        {nav}
+        {joinButton}
+      </MobileMenu>
+    </MobileNav>
+  );
+
+  const desktopNav = (
+    <DesktopNav>
+      <Container>
+        <Wordmark>
+          <a href="/">
+            Cabin
+          </a>
+        </Wordmark>
+        <Links>
+          {nav}
+        </Links>
+        {joinButton}
+      </Container>
+    </DesktopNav>
+  );
+
+  return (
+    <Bar>
+      <Wrapper>
+        {mobileNav}
+        {desktopNav}
+      </Wrapper>
+    </Bar>
+  )
+}
+
+const Bar = styled('header', {
+  display: 'flex',
+  alignItems: 'center',
+  backgroundColor: '$sand',
+  borderBottom: '1px solid $sprout',
+  '@md': {
+    border: 'none',
+  },
+});
+
+const MobileNav = styled('nav', {
+  display: 'flex',
+  flexDirection: 'column',
+  '@md': {
+    display: 'none',
+  },
+});
+
+const DesktopNav = styled('div', {
+  display: 'none',
+  '@md': {
+    display: 'flex',
+  },
+});
+
+const Container = styled('div', {
+  height: '4rem',
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  '@md': {
+    height: '$20',
+  },
+});
+
+const Wordmark = styled('div', {
+  fontSize: '$xxl',
+  m: '0',
+  a: {
+    color: 'inherit',
+    textDecoration: 'none',
+    fontFamily: "$sans",
+    fontWeight: 600,
+  },
+  '@md': {
+    fontSize: '$xxxl',
+    justifyContent: 'center',
+  },
+});
+
+const Links = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+});
+
+const Nav = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  fontFamily: '$mono',
+  fontSize: '$sm',
+  textTransform: 'uppercase',
+  mb: '$4',
+  a: {
+    color: 'inherit',
+    textDecoration: 'none',
+    display: 'flex',
+    height: '$10',
+    px: '$4',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '&:hover': { 
+      textDecoration: 'underline',
+    },
+  },
+  '@md': {
+    ml: '$6',
+    mb: '0',
+    flexDirection: 'row',
+  },
+});
+
+const MenuToggle = styled('button', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '$10',
+  width: '$10',
+  border: 'none',
+  background: 'none',
+  svg: {
+    height: '$6',
+    width: '$6',
+    color: '$forest',
+  },
+  '@md': {
+    display: 'none',
+  },
+});
+
+const MobileMenu = styled('div', {
+  display: 'none',
+  flexDirection: 'column',
+  alignItems: 'center',
+  mb: '$6',
+  variants: { 
+    open: {
+      true : {
+        display: 'flex',
+      },
+    },
+  },
+});
