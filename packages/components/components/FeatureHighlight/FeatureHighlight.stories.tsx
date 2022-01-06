ce57bddbcf21f702ac02602cd0ff3df10febb483
DugDaniels <cdouglasd@gmail.com>
2022-01-06 10:23:28 -0500

    Add FeatureHighlight component

---
+++ packages/components/components/FeatureHighlight/FeatureHighlight.stories.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import { Story } from "@storybook/react";
+import FeatureHighlight, { IFeatureHighlightProps } from "./FeatureHighlight";
+import { Box } from "../";
+
+export default {
+  title: "Components/FeatureHighlight",
+  component: FeatureHighlight,
+  argTypes: {},
+} as Meta;
+
+type FeatureHighlightArgs = Parameters<typeof FeatureHighlight>[0];
+
+const Template: Story<FeatureHighlightArgs> = (args) => (
+  <Box>
+    <FeatureHighlight {...args} />
+  </Box>
+);
+
+export const Standard = Template.bind({});
+Standard.args = {
+  heading: "Hello Topo",
+  body: "Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.",
+  BackgroundImageComponent: (props) => (
+    <img 
+      {...props}
+      alt={"A real view of a cabin"}
+      src={
+        "https://via.placeholder.com/984x464"
+      }
+    />
+  ),
+  ImageComponent: (props) => (
+    <img
+      {...props}
+      alt="A real view of a cabin"
+      src="https://via.placeholder.com/492x327"
+      width={492} 
+      height={327}
+    />
+  ),
+};
+
+export const Reversed = Template.bind({});
+Reversed.args = {
+  heading: "Hello Topo",
+  body: "Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.",
+  BackgroundImageComponent: (props) => (
+    <img 
+      {...props}
+      alt={"A real view of a cabin"}
+      src={
+        "https://via.placeholder.com/984x464"
+      }
+    />
+  ),
+  ImageComponent: (props) => (
+    <img
+      {...props}
+      alt="A real view of a cabin"
+      src="https://via.placeholder.com/492x327"
+      width={492} 
+      height={327}
+    />
+  ),
+  reversed: true,
+};
