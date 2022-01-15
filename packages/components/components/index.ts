0a9a9232f355439fb730cf99008a1d1d0f1e815e
Joe Hsu <jhsu.x1@gmail.com>
2022-01-15 10:18:53 -0500

    chore: export components from index

--- packages/components/components/index.ts
+++ packages/components/components/index.ts
@@ -1,10 +1,17 @@
 import Heading from "./Heading";
+import Navigation from "./Navigation";
 
 import Text from "./Text";
 
+import Blurb from "./Blurb";
 import Box from "./Box";
 import Button from "./Button";
+import CallToAction from "./CallToAction";
+import Checkbox from "./Checkbox";
 import ConvertKit from "./ConvertKit";
+import FeatureHighlight from "./FeatureHighlight";
+import Footer from "./Footer";
+import HandleTag from "./HandleTag";
 import Hero from "./Hero";
 import Input from "./Input";
 import Label from "./Label";
@@ -18,14 +25,20 @@ export {
 
   Text,
 
+  Blurb,
   Box,
   Button,
+  Checkbox,
   ConvertKit,
+  FeatureHighlight,
+  Footer,
+  HandleTag,
   Hero,
   Input,
   Label,
 
   Modal,
+  Navigation,
   Textarea,
   Wrapper,
 };
