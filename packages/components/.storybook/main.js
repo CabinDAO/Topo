3c35512701bad1b96a59dddc7f637a770bbbf02f
David Vargas <dvargas92495@gmail.com>
2022-02-01 19:03:16 -0500

    New Toast component for System feedback (#26)
    * New Toast component for System feedback
    
    * version bump

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,5 +1,29 @@
+const path = require("path");
+const pathToInlineSvg = path.resolve(__dirname, "../assets");
+
 module.exports = {
   stories: ["../components/**/*.stories.tsx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: "@storybook/react",
+  webpackFinal: async (config) => {
+    const rules = config.module.rules;
+
+    // modify storybook's file-loader rule to avoid conflicts with svgr
+    const fileLoaderRule = rules.find((rule) => rule.test.test(".svg"));
+    fileLoaderRule.exclude = pathToInlineSvg;
+
+    rules.push({
+      test: /\.svg$/,
+      include: pathToInlineSvg,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
---
+++ packages/components/components/Toast/Toast.stories.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import { Story } from "@storybook/react";
+import Toast from "./Toast";
+import { Box } from "../";
+import Button, { IButtonProps } from "../Button/Button";
+
+export default {
+  title: "Components/Toast",
+  component: Toast,
+  argTypes: {},
+  args: {},
+} as Meta;
+
+const ToastTemplate = (
+  args: Parameters<typeof Toast>[0] & Pick<IButtonProps, "type">
+) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setIsOpen(true)} type={args.type}>
+        OPEN {args.intent?.toUpperCase?.()}
+      </Button>
+      <Toast {...args} isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
+  );
+};
+
+const Template: Story<Parameters<typeof Toast>[0]> = (args) => {
+  return (
+    <Box
+      css={{ width: "100%", display: "flex", justifyContent: "space-between" }}
+    >
+      <ToastTemplate type={"primary"} intent={"success"} {...args} />
+      <ToastTemplate type={"link"} intent={"warning"} {...args} />
+      <ToastTemplate type={"secondary"} intent={"error"} {...args} />
+    </Box>
+  );
+};
+
+export const Standard = Template.bind({});
+Standard.args = { message: "Status Message" };
+
+export const ShowClose = Template.bind({});
+ShowClose.args = {
+  message: "Status Message",
+  showCloseIcon: true,
+};
+
+export const ShowAction = Template.bind({});
+ShowClose.args = {
+  message: "Status Message",
+  showCloseIcon: true,
+  actions: [
+    {
+      text: "Action",
+      onClick: () => console.log("Clicked an action!"),
+    },
+  ],
+};
+
+export const StatusTitle = Template.bind({});
+StatusTitle.args = {
+  message: "Status Message",
+  title: "Status Title",
+};
+
+export const ShowTitleAndClose = Template.bind({});
+ShowTitleAndClose.args = {
+  showCloseIcon: true,
+  title: "Status Title",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+};
+
+export const ShowTitleAndAction = Template.bind({});
+ShowTitleAndAction.args = {
+  title: "Status Title",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  actions: [
+    {
+      text: "Action",
+      onClick: () => console.log("Clicked an action!"),
+    },
+  ],
+};
+
+export const ShowSecondaryAction = Template.bind({});
+ShowSecondaryAction.args = {
+  title: "Status Title",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  actions: [
+    {
+      text: "Action",
+      onClick: () => console.log("Clicked an action!"),
+    },
+    {
+      text: "Action",
+      onClick: () => console.log("Clicked a second action!"),
+    },
+  ],
+};
